29b001975c3d25ec4a2ec6cdb2f989ed717a8971
Nazarshia2889 <arshia.nayebnazar@yahoo.com>
2022-08-14 11:28:09 -0700

    Encryption done

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,25 @@
 package _01_File_Recorder;
 
-public class FileRecorder {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileRecorder { 
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		try {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Write a message to save to the file.");
+			String line = scanner.nextLine();
+			scanner.close();
+			
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/text3");
+			
+			fw.write("Hello");
+			
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,5 +1,9 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileEncryptor {
 	/*
 	 * Encryption is the process of encoding a message or information
@@ -20,4 +24,38 @@ public class FileEncryptor {
 	 * Create a program that takes a message and a key from the user.
 	 * Use the key to shift each letter in the users input and save the final result to a file.
 	 */
+	
+	public static void main(String[] args) {
+//		char[] arr = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a message:");
+		String message = scanner.nextLine();
+		System.out.println("Enter a key:");
+		String key = scanner.nextLine();
+		int keyInt = Integer.parseInt(key);
+		char[] messageArr = message.toCharArray();
+		for(int i = 0; i < messageArr.length; i++) {
+			if(messageArr[i] == ' ') {
+				continue;
+			}
+			
+			char newChar = (char)((messageArr[i] + keyInt));
+			
+			if((((messageArr[i] + keyInt) > 90) && (messageArr[i] == Character.toUpperCase(messageArr[i]))) ||
+					((messageArr[i] + keyInt) > 122) && (messageArr[i] != Character.toUpperCase(messageArr[i]))) {
+				newChar = (char)((messageArr[i] + keyInt) - 26);
+			}
+			messageArr[i] = newChar;
+		}
+		scanner.close();
+		try {
+			FileWriter fw = new FileWriter("src/_02_File_Encrypt_Decrypt/encryption.txt");
+			fw.write(messageArr);
+			
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
